f606a8029604c4dd4e78b382f63a3147074ae656
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-10-24 06:29:56 +0000

    Made Changes in events page and recreted

--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import {NavBar} from './NavBar'
-
+import Parallax1 from './Parallax1'
+import { Footer } from "./Footer";
+import { Contact } from "./Contact";
 
 const Event = () => {
   return (
     <div>
     <NavBar/>
+    <Parallax1/>
+    <Contact/>
+    <Footer/>
 
     </div>
   )
