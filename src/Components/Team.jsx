d6617cf33063580288fa28f99c9225b3903b8154
Spandan Mukherjee <95229740+SpandanM110@users.noreply.github.com>
2023-10-24 06:30:17 +0000

    Updated the teams page

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -20,7 +20,7 @@ class Team extends React.Component {
                 className="row-me justify-content-center"
                 style={{ marginBottom: "2%" }}
               >
-                <h1 className="heading">Core Team</h1>
+                <h1 className="heading">Core Team and Moderators</h1>
               </div>
               <div className="row-me">
                 {/* Team members - You can repeat this structure for each team member */}
@@ -108,98 +108,7 @@ class Team extends React.Component {
             </div>
           </div>
 
-          {/* Moderators */}
-          <div className="team-wrapper card">
-            <div
-              className="row-me justify-content-center"
-              style={{ marginBottom: "2%" }}
-            >
-              <p className="heading">Moderators</p>
-            </div>
-            <div className="row-me">
-              {/* Team members - You can repeat this structure for each team member */}
-              <div className="col-md">
-                <div className="container-fluid">
-                  <div className="dp">
-                    <img
-                      src={shree}
-                      style={{ objectFit: "cover" }}
-                      width="100%"
-                      height="100%"
-                    />
-                  </div>
-                  <div id="brief">
-                    <p id="name">Prof. Doctor Strange</p>
-                    <p id="designation">Community Head (Faculty)</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="container-fluid">
-                  <div className="dp">
-                    <img
-                      src={shree}
-                      style={{ objectFit: "cover" }}
-                      width="100%"
-                      height="100%"
-                    />
-                  </div>
-                  <div id="brief">
-                    <p id="name">Shreeniwas Chaudhari</p>
-                    <p id="designation">Community Leader</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="container-fluid">
-                  <div className="dp">
-                    <img
-                      src={prajak}
-                      style={{ objectFit: "cover" }}
-                      width="100%"
-                      height="100%"
-                    />
-                  </div>
-                  <div id="brief">
-                    <p id="name">Prajakta Patil</p>
-                    <p id="designation">Community Leader</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="container-fluid">
-                  <div className="dp">
-                    <img
-                      src={prasad}
-                      style={{ objectFit: "cover" }}
-                      width="100%"
-                      height="100%"
-                    />
-                  </div>
-                  <div id="brief">
-                    <p id="name">Prasad Chavan</p>
-                    <p id="designation">Community Manager</p>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md">
-                <div className="container-fluid">
-                  <div className="dp">
-                    <img
-                      src={harry}
-                      style={{ objectFit: "cover" }}
-                      width="100%"
-                      height="100%"
-                    />
-                  </div>
-                  <div id="brief">
-                    <p id="name">Harsh Patil</p>
-                    <p id="designation">Community Manager</p>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
         {/* Footer */}
         <div className="container-fluid" id="space">
